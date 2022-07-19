2dbedf701418420741f958d2cb7309891beefee4
Andy8787(布長) <andy8787main@gmail.com>
2022-07-19 08:23:05 +0800

    print

--- (M)auto_left_mouse.py
+++ (M)auto_left_mouse.py
@@ -9,18 +9,22 @@ time.sleep(2)
 flag = True
 
 while True:
-    if (keyboard.read_key() == "`"):
+    if (keyboard.read_key() == "."):
         if (flag):
-            pydirectinput.mouseDown()
-            pydirectinput.mouseDown(button='right')
-            # pyautogui.keyDown("space")
+            # pydirectinput.mouseDown()
+            # pydirectinput.mouseDown(button='right')
+            pyautogui.keyDown("space")
+            print("On")
+            # pyautogui.keyDown("w")
             time.sleep(0.1)
             flag = False
             time.sleep(0.1)
         else:
-            pydirectinput.mouseUp()
-            pydirectinput.mouseUp(button='right')
-            # pyautogui.keyUp("space")
+            # pydirectinput.mouseUp()
+            # pydirectinput.mouseUp(button='right')
+            pyautogui.keyUp("space")
+            print("Off")
+            # pyautogui.keyUp("w")
             time.sleep(0.1)
             flag = True
             time.sleep(0.1)
